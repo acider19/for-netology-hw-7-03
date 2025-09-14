37abb2108e40718ba2eb57c04ff4fa7c1010e3f8
acider19 <acider19@gmail.com>
2025-09-14 21:44:31 +0300

    Add vars

--- variables.tf
+++ variables.tf
@@ -3,3 +3,12 @@ variable "flow" {
   type    = string
   default = "41"
 }
+variable "cloud_id" {
+  type    = string
+  default = "b1g68t7p4rjq035vq0o9"
+}
+
+variable "folder_id" {
+  type    = string
+  default = "b1g2ett5h4qn2kbsh6gr"
+}
